601b0f0caf2ad0557e62eff6e046323d320a52d7
Guru Srikar Thopugunta <gurusrikar@gmail.com>
2017-05-09 12:33:47 -0700

    added array support in resulting json

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 })();
 
+var testCasesList = ['', ''];
+
 var ractive = new Ractive({
     // The `el` option can be a node, an ID, or a CSS selector.
     el: '#form-container',
@@ -14,12 +16,13 @@ var ractive = new Ractive({
     // We could pass in a string, but for the sake of convenience
     // we're passing the ID of the <script> tag above.
     template: '#template',
+    modifyArrays: true,
 
     // Here, we're passing in some initial data
     data: {
         lang: 'Python',
         code: '',
-        testCases: '',
+        testCasesList: testCasesList,
         hiddenCode: '',
         score: '0'
     }
@@ -34,24 +37,28 @@ function saveJson() {
     return false;
 }
 
+function addTestCase() {
+    console.log("adding new test case");
+    testCasesList.push('');
+    // ractive.set('testCasesList', ractive.get('testCasesList').push(''));
+    return false;
+}
+
 function updateJSON() {
     var outputElement = document.getElementById("output-json");
     outputElement.innerHTML = JSON.stringify(ractive.get(), undefined, 4);
 }
 
 function downloadJson() {
+    var fileName = (document.getElementById("inputFileName").value || "sample") + ".json";
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(ractive.get(), undefined, 4));
     var downloadSimulateElem = document.getElementById("hiddenDownloadElem");
     downloadSimulateElem.setAttribute("href", dataStr);
-    downloadSimulateElem.setAttribute("download", "sample.json");
+    downloadSimulateElem.setAttribute("download", fileName);
     downloadSimulateElem.click();
 }
 
 setInterval(function () {
-    console.log("outside");
-    if (JSON.stringify(previousUpdate) != JSON.stringify(ractive.get())) {
-        console.log("inside");
-        previousUpdate = ractive.get();
-        updateJSON();
-    }
+    previousUpdate = ractive.get();
+    updateJSON();
 }, 1500);
